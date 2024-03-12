86aad3b9964b131f84f480bd2ba1224a270c0b36
Namnpse <nam444479@gmail.com>
2024-03-12 22:06:48 +0700

    [Android] update dependencies in build_gradle file

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -37,19 +37,22 @@ android {
 }
 
 dependencies {
+    val compose_version= "1.6.3"
+    val hilt_android = "2.49"
+    val hilt_compose = "1.2.0"
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
-    implementation("androidx.compose.foundation:foundation:1.2.1")
-    implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+    implementation("androidx.compose.ui:ui:$compose_version")
+    implementation("androidx.compose.ui:ui-tooling:$compose_version")
+    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
+    implementation("androidx.compose.foundation:foundation:$compose_version")
+    implementation("androidx.compose.material:material:$compose_version")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
-    implementation("com.google.dagger:hilt-android:2.42")
-    kapt("com.google.dagger:hilt-android-compiler:2.42")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("com.google.dagger:hilt-android:$hilt_android")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt_android")
+    kapt("androidx.hilt:hilt-compiler:$hilt_compose")
+    implementation("androidx.hilt:hilt-navigation-compose:$hilt_compose")
 }
