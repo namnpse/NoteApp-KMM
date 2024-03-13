0da54117105b33cf2bd438681c3e370b588b5523
Namnpse <nam444479@gmail.com>
2024-03-13 22:27:19 +0700

    [Android] add navigation, navigate from list note to detailed note

--- androidApp/src/main/java/com/namnpse/noteapp_kmp/android/MainActivity.kt
+++ androidApp/src/main/java/com/namnpse/noteapp_kmp/android/MainActivity.kt
@@ -3,14 +3,13 @@ package com.namnpse.noteapp_kmp.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.namnpse.noteapp_kmp.presentation.Greeting
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.namnpse.noteapp_kmp.android.detailed_note.NoteDetailScreen
+import com.namnpse.noteapp_kmp.android.note_list.NoteListScreen
 import com.namnpse.noteapp_kmp.android.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +17,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NoteAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting(Greeting().greeting())
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "note_list") {
+
+                    composable(route = "note_list") {
+                        NoteListScreen(navController = navController)
+                    }
+
+                    composable(
+                        route = "note_detail/{noteId}",
+                        arguments = listOf(
+                            navArgument(name = "noteId") {
+                                type = NavType.LongType
+                                defaultValue = -1L
+                            }
+                        )
+                    ) { backStackEntry ->
+                        val noteId = backStackEntry.arguments?.getLong("noteId") ?: -1L
+                        NoteDetailScreen(
+                            navController = navController,
+                            noteId = noteId,
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    NoteAppTheme {
-        Greeting("Hello, Android!")
     }
 }
---
+++ androidApp/src/main/java/com/namnpse/noteapp_kmp/android/detailed_note/NoteDetailScreen.kt
@@ -0,0 +1,47 @@
+package com.namnpse.noteapp_kmp.android.detailed_note
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun NoteDetailScreen(
+    noteId: Long,
+    navController: NavController,
+    viewModel: NoteDetailViewModel = hiltViewModel()
+) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                backgroundColor = Color.Black
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Check,
+                    contentDescription = "Save note",
+                    tint = Color.White
+                )
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+
+        }
+    }
+}
