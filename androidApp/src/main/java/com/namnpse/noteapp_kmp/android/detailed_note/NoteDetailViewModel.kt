a718ef348cd01ec6aa76cbe37f07bfd52d2a77da
Namnpse <nam444479@gmail.com>
2024-03-13 19:43:25 +0700

    [Android] handle get detailed note, save note

---
+++ androidApp/src/main/java/com/namnpse/noteapp_kmp/android/detailed_note/NoteDetailViewModel.kt
@@ -0,0 +1,98 @@
+package com.namnpse.noteapp_kmp.android.detailed_note
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.namnpse.noteapp_kmp.domain.note.Note
+import com.namnpse.noteapp_kmp.domain.note.NoteDataSource
+import com.namnpse.noteapp_kmp.domain.utils.DateTimeUtil
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteDetailViewModel @Inject constructor(
+    private val noteDataSource: NoteDataSource,
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+    private val noteTitle = savedStateHandle.getStateFlow("noteTitle", "")
+    private val isNoteTitleFocused = savedStateHandle.getStateFlow("isNoteTitleFocused", false)
+    private val noteContent = savedStateHandle.getStateFlow("noteContent", "")
+    private val noteType = savedStateHandle.getStateFlow("noteType", "")
+    private val isNoteContentFocused = savedStateHandle.getStateFlow("isNoteContentFocused", false)
+    private val noteColor = savedStateHandle.getStateFlow(
+        "noteColor",
+        Note.getRandomColor()
+    )
+
+    val detailedNoteState = combine(
+        noteTitle,
+        isNoteTitleFocused,
+        noteContent,
+        isNoteContentFocused,
+        noteColor
+    ) { title, isTitleFocused, content, isContentFocused, color ->
+        NoteDetailState(
+            noteTitle = title,
+            isNoteTitleHintVisible = title.isEmpty() && !isTitleFocused,
+            noteContent = content,
+            isNoteContentHintVisible = content.isEmpty() && !isContentFocused,
+            noteColor = color
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), NoteDetailState())
+
+    private val _hasNoteBeenSaved = MutableStateFlow(false)
+    val hasNoteBeenSaved = _hasNoteBeenSaved.asStateFlow()
+
+    private var existingNoteId: Long? = null
+
+    init {
+        savedStateHandle.get<Long>("noteId")?.let { existingNoteId ->
+            if(existingNoteId == -1L) {
+                return@let
+            }
+            this.existingNoteId = existingNoteId
+            viewModelScope.launch {
+                noteDataSource.getNoteById(existingNoteId)?.let { note ->
+                    savedStateHandle["noteTitle"] = note.title
+                    savedStateHandle["noteContent"] = note.content
+                    savedStateHandle["noteColor"] = note.hexColor
+                }
+            }
+        }
+    }
+
+    fun onNoteTitleChanged(text: String) {
+        savedStateHandle["noteTitle"] = text
+    }
+
+    fun onNoteContentChanged(text: String) {
+        savedStateHandle["noteContent"] = text
+    }
+
+    fun onNoteTitleFocusChanged(isFocused: Boolean) {
+        savedStateHandle["isNoteTitleFocused"] = isFocused
+    }
+
+    fun onNoteContentFocusChanged(isFocused: Boolean) {
+        savedStateHandle["isNoteContentFocused"] = isFocused
+    }
+
+    fun saveNote() {
+        viewModelScope.launch {
+            noteDataSource.insertNote(
+                Note(
+                    id = existingNoteId,
+                    title = noteTitle.value,
+                    content = noteContent.value,
+                    type = noteType.value,
+                    hexColor = noteColor.value,
+                    createdAt = DateTimeUtil.now()
+                )
+            )
+            _hasNoteBeenSaved.value = true
+        }
+    }
+}
