ec6ac7e57850d3a2a7a1b69b6f1fec3df7d80bf6
Namnpse <nam444479@gmail.com>
2024-03-14 09:19:00 +0700

    [Android] build detailed note screen

--- androidApp/src/main/java/com/namnpse/noteapp_kmp/android/detailed_note/NoteDetailScreen.kt
+++ androidApp/src/main/java/com/namnpse/noteapp_kmp/android/detailed_note/NoteDetailScreen.kt
@@ -1,19 +1,24 @@
 package com.namnpse.noteapp_kmp.android.detailed_note
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.namnpse.noteapp_kmp.android.common.TransparentTextField
 
 @Composable
 fun NoteDetailScreen(
@@ -21,10 +26,19 @@ fun NoteDetailScreen(
     navController: NavController,
     viewModel: NoteDetailViewModel = hiltViewModel()
 ) {
+    val state by viewModel.detailedNoteState.collectAsState()
+    val hasNoteBeenSaved by viewModel.hasNoteBeenSaved.collectAsState()
+
+    LaunchedEffect(key1 = hasNoteBeenSaved) {
+        if(hasNoteBeenSaved) {
+            navController.popBackStack()
+        }
+    }
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {},
+                onClick = viewModel::saveNote,
                 backgroundColor = Color.Black
             ) {
                 Icon(
@@ -37,11 +51,35 @@ fun NoteDetailScreen(
     ) { padding ->
         Column(
             modifier = Modifier
+                .background(Color(state.noteColor))
                 .fillMaxSize()
                 .padding(padding)
                 .padding(16.dp)
         ) {
-
+            TransparentTextField(
+                text = state.noteTitle,
+                hint = "Enter a title...",
+                isHintVisible = state.isNoteTitleHintVisible,
+                onValueChanged = viewModel::onNoteTitleChanged,
+                onFocusChanged = {
+                    viewModel.onNoteTitleFocusChanged(it.isFocused)
+                },
+                singleLine = true,
+                textStyle = TextStyle(fontSize = 20.sp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TransparentTextField(
+                text = state.noteContent,
+                hint = "Enter some content...",
+                isHintVisible = state.isNoteContentHintVisible,
+                onValueChanged = viewModel::onNoteContentChanged,
+                onFocusChanged = {
+                    viewModel.onNoteContentFocusChanged(it.isFocused)
+                },
+                singleLine = false,
+                textStyle = TextStyle(fontSize = 20.sp),
+                modifier = Modifier.weight(1f)
+            )
         }
     }
 }
